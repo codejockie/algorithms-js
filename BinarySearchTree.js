ee7114ec5c1824e34a0e15ff47bf900e9632d088
John C. Kennedy <devjckennedy@gmail.com>
2019-12-10 12:34:10 +0000

    remove unwanted code

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -41,16 +41,6 @@ class BinarySearchTree {
   }
 
   insert(data) {
-    // if (data < this.data && this.left) {
-    //   this.left.insert(data);
-    // } else if (data < this.data) {
-    //   this.left = new Node(data);
-    // } else if (data > this.data && this.right) {
-    //   this.right.insert(data);
-    // } else if (data > this.data) {
-    //   this.right = new Node(data);
-    // }
-
     const node = new Node(data, null, null);
     var currentNode;
 
